c8644b93c9097a86c96a8ae271b450ff7476f85d
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-03-22 23:54:21 +0100

    corrected orderresponse to contain productids instead of whole products

--- ECommerce.Contracts/Responses/OrderResponse.cs
+++ ECommerce.Contracts/Responses/OrderResponse.cs
@@ -4,7 +4,7 @@ public class OrderResponse
 {
     public required Guid Id { get; init; }
     public required Guid CustomerId { get; init; }
-    public required IEnumerable<Guid> ProductIds { get; init; }
+    public required List<ProductResponse> Products { get; init; }
     public required DateTime OrderDate { get; init; }
     public required string Status { get; init; }
 }
