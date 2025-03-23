85da0653326784b566d35eecbe3ec909b0fe7345
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-03-23 20:06:02 +0100

    added controller option to ignore cyclic references

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using ECommerce.Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,10 @@ var config = builder.Configuration;
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+       options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); //ignore cyclic ref on create method in controller
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
