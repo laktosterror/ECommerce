2fca18ec4e3b8ff9041a2514f189f37f7495098e
laktosterror <55037659+laktosterror@users.noreply.github.com>
2025-03-31 22:52:01 +0200

    code reformat and cleanup

--- ECommerce.Client/Services/ICustomerService.cs
+++ ECommerce.Client/Services/ICustomerService.cs
@@ -1,15 +1,14 @@
+using ECommerce.Api.Mapping;
 using ECommerce.Contracts.Requests;
 using ECommerce.Contracts.Responses;
-using ECommerce.Api.Mapping;
 
-namespace ECommerce.Client.Services
+namespace ECommerce.Client.Services;
+
+public interface ICustomerService
 {
-    public interface ICustomerService
-    {
-        Task<CustomersResponse> GetCustomersAsync();
-        Task<CustomerResponse> GetCustomerAsync(Guid id);
-        Task<CustomerResponse> CreateCustomerAsync(CreateCustomerRequest request);
-        Task<CustomerResponse> UpdateCustomerAsync(Guid id, UpdateCustomerRequest request);
-        Task DeleteCustomerAsync(Guid id);
-    }
+    Task<CustomersResponse> GetCustomersAsync();
+    Task<CustomerResponse> GetCustomerAsync(Guid id);
+    Task<CustomerResponse> CreateCustomerAsync(CreateCustomerRequest request);
+    Task<CustomerResponse> UpdateCustomerAsync(Guid id, UpdateCustomerRequest request);
+    Task DeleteCustomerAsync(Guid id);
 }
--- ECommerce.Client/Services/OrderService.cs
+++ ECommerce.Client/Services/OrderService.cs
@@ -1,4 +1,3 @@
-using System.Net.Http.Json;
 using ECommerce.Contracts.Requests;
 using ECommerce.Contracts.Responses;
 
